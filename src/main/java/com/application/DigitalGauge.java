c3098c5d90f899308519004ceaaf7093624828ab
jsildatk <jakub.sildatk@gmail.com>
2018-11-10 22:32:43 +0100

    Complete layout

---
+++ src/main/java/com/application/DigitalGauge.java
@@ -0,0 +1,77 @@
+package com.application;
+
+import javafx.geometry.VPos;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.effect.Lighting;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+
+public class DigitalGauge extends Parent {
+	private final double boxHeight = 15;
+	private final double boxWidth = boxHeight * 5 / 7;
+	private final double scale = 5;
+	private final Font font = new Font(10 * scale);
+	private HBox hBox = new HBox();
+	private Text[] digits;
+	private Group[] digitsGroup;
+	
+	public DigitalGauge(int size) {
+		digits = new Text[size];
+		digitsGroup = new Group[size];
+		configureDigits();
+		configureHbox();
+		getChildren().add(hBox);
+	}
+	
+	private void configureHbox() {
+		hBox.getChildren().addAll(digitsGroup);
+		hBox.setSpacing(2);
+	}
+	
+	private void configureDigits() {
+		for (int i = 0; i < digits.length; i++) {
+			digits[i] = new Text(" ");
+			digits[i].setFont(font);
+			digits[i].setTextOrigin(VPos.TOP);
+			digits[i].setLayoutY(-10);
+			Rectangle bg = null;
+			bg = createBackground(Color.ANTIQUEWHITE, Color.BLACK);
+			digits[i].setFill(Color.BLACK);
+			digitsGroup[i] = new Group(bg, digits[i]);
+		}
+	}
+
+	private Rectangle createBackground(Color fill, Color stroke) {
+		Rectangle bg = new Rectangle(boxWidth * scale, boxHeight * scale, fill);
+		bg.setStroke(stroke);
+		bg.setStrokeWidth(3);
+		bg.setEffect(new Lighting());
+		return bg;
+	}
+
+	public void refreshDigits(String number) {
+		if (number.length() == 1) {
+			digits[digits.length-1].setText(number);
+		} else if (number.length() == 2) {
+			digits[digits.length-1].setText(number.substring(1, 2));
+			digits[digits.length-2].setText(number.substring(0, 1));
+		} else if (number.length() == 3) {
+			digits[digits.length-1].setText(number.substring(2, 3));
+			digits[digits.length-2].setText(number.substring(1, 2));
+			digits[digits.length-3].setText(number.substring(0, 1));
+		} else if (number.length() == 4) {
+			digits[digits.length-1].setText(number.substring(3, 4));
+			digits[digits.length-2].setText(number.substring(2, 3));
+			digits[digits.length-3].setText(number.substring(1, 2));
+			digits[digits.length-4].setText(number.substring(0, 1));
+		} else {
+			for (int i = 0; i < number.length(); i++) {
+				digits[i].setText(" ");
+			}
+		}
+	}
+}
---
+++ src/main/java/com/application/controller/MainController.java
@@ -0,0 +1,40 @@
+package com.application.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
+
+import com.application.DigitalGauge;
+
+public class MainController implements Initializable {
+	@FXML
+	private StackPane stackPaneGaugeKMH;
+	@FXML
+	private StackPane stackPaneGaugeRPM;
+	@FXML
+	private StackPane stackPaneGaugeGear;
+	@FXML
+	private StackPane stackPaneGaugeShift;
+	
+	private DigitalGauge gaugeKMH;
+	private DigitalGauge gaugeRPM;
+	private DigitalGauge gaugeGear;
+	private DigitalGauge gaugeShift;
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		gaugeKMH = new DigitalGauge(3);
+		gaugeRPM = new DigitalGauge(4);
+		gaugeGear = new DigitalGauge(1);
+		gaugeShift = new DigitalGauge(1);
+		stackPaneGaugeKMH.getChildren().add(gaugeKMH);
+		stackPaneGaugeRPM.getChildren().add(gaugeRPM);
+		stackPaneGaugeGear.getChildren().add(gaugeGear);
+		stackPaneGaugeShift.getChildren().add(gaugeShift);
+	}
+
+}
