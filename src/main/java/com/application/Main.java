268a538e9ec0c114efeb1a90893300617ab55aa8
jsildatk <jakub.sildatk@gmail.com>
2018-11-11 13:53:52 +0100

    Application icon

--- src/main/java/com/application/Main.java
+++ src/main/java/com/application/Main.java
@@ -1,8 +1,12 @@
 package com.application;
 	
+import java.io.FileInputStream;
+import java.net.URL;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
@@ -13,6 +17,7 @@ public class Main extends Application {
 			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
 			Scene scene = new Scene(root,800,600);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.getIcons().add(new Image(Main.class.getResource("images/icon.png").toExternalForm()));
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("Need For Speed");
 			primaryStage.show();
